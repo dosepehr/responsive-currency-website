25f7f2d57bea8f3a870ca373856bf38d33b56dbf
sepehr <dosepehr@gmail.com>
2022-11-17 17:54:51 +0400

    navbar is done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
+//hamburger menu
 const ham = document.querySelector('.ham');
-
+const nav = document.querySelector('.menu');
 
 ham.addEventListener('click', function () {
-    ham.classList.toggle('open');
+    ham.classList.toggle('active');
+    nav.classList.toggle('open');
+    if (nav.classList.contains('open')) {
+        nav.style.height = nav.scrollHeight + "px"
+    } else {
+        nav.style.height = ""
+    }
 });
